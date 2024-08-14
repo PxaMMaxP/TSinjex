3c308635dd3e9fb33a2721574477d2fb66129dad
Max P <Mail@MPassarello.de>
2024-08-14 21:39:59 +0200

    Standardize file naming and improve typings export
    Renamed functions files to follow consistent camelCase convention. Added a missing type export for ITSInjex interface in the main index file, enabling better TypeScript type support and ensuring interfaces are available for external modules.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 export * from './TSInjex';
-
 export * from './decorators/Inject';
 export * from './decorators/Register';
 export * from './decorators/RegisterInstance';
-
 export * from './functions/resolve';
 export * from './functions/register';
+export type * from './interfaces/ITSInjex';
