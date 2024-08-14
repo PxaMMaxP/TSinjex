c6ba0b7801dd94b915928e4e7626c20a461db7ce
Max P <Mail@MPassarello.de>
2024-08-14 20:50:09 +0200

    Update version and expand exports
    Upgraded package version from 0.0.1 to 0.0.2 to reflect new functionality. Added exports for `resolve` and `register` functions to extend the library capabilities.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export * from './TSInjex';
 export * from './decorators/Inject';
 export * from './decorators/Register';
 export * from './decorators/RegisterInstance';
+
+export * from './functions/resolve';
+export * from './functions/register';
