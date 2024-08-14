de2cca17427bca9b87341e37f31e46afcb7c647d
Max P <Mail@MPassarello.de>
2024-08-14 23:20:44 +0200

    Bump version to 0.0.7, export additional types
    Updated package version to 0.0.7 to reflect new changes. Added export statements for `InitDelegate` and `GenericConstructor` types, enhancing type availability for package users.

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,5 @@ export * from './decorators/RegisterInstance';
 export * from './functions/resolve';
 export * from './functions/register';
 export type * from './interfaces/ITSinjex';
+export type * from './types/InitDelegate';
+export type * from './types/GenericContructor';
